aa7a67a20d465733f11827bd7de5b84a76f9aa8d
klings <klingsen@gmail.com>
2013-03-24 22:30:44 +0100

    Fixed GUI, and added some content

---
+++ KlingsDotOrg/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Optimization;
+
+namespace KlingsDotOrg.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
+        }
+    }
+}
